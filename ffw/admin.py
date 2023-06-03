76d652fc30b5b3e254a18a4beec0f8a3005bbfe7
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-06-03 20:07:13 +0000

    Add basid models for Plan and Occupation and register them with admin

--- ffw/admin.py
+++ ffw/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Plan, Occupation
 
 # Register your models here.
+
+admin.site.register(Plan)
+admin.site.register(Occupation)
--- ffw/models.py
+++ ffw/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Occupation(models.Model):
+    name = models.CharField(max_length=350) #programmatic way to find in views.py and elsewhere
+    friendly_name = models.CharField(max_length=350, null=True, blank=True) #nicer
+
+    def __str__(self):
+        return self.name #just returns products name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Plan(models.Model):
+    occupation = models.ForeignKey('Occupation', null="True", blank="True", on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=300, null=True, blank=True)
+    name = models.CharField(max_length=300)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    difficulty = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=2000, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+
+#Not blank=true, null=true, each plan requires a name, description, price, everything else is optional
+    def __str__(self):
+        return self.name #just returns products name
