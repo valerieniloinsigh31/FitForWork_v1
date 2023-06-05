e8def0b7d1f7e51b255b3d607cb7e11b12821fd1
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-06-05 17:49:38 +0000

    Update context processor to declare grant_total variable so shopping bag tally works

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,26 +1,46 @@
 from decimal import Decimal 
 from django.conf import settings 
+from django.shortcuts import get_object_or_404
+from plans.models import Plan
+
 
 def bag_contents(request):
 
     bag_items = []
-    total = 0 #intialise to 0
-    plan_count = 0 #intialise to 0
+    total = 0 #initialise to 0
+    plan_count = 0 #initialise to 0
+    bag = request.session.get('bag', {}) #same as in add_to_bag view. Gets bag in session if exists, initializing to empty dictionary if not
 
-    if total < settings.FREE_PT_THRESHOLD:
+    #in order to populate values of variables we are not using yet, we need to iterate through all of the items in 
+    #the bag and along the way tally up total cost and plan count and add
+    #plans and data to bag_items list to display throughout site and on bag page
 
-        free_pt_delta = settings.FREE_PT_THRESHOLD - total #Allows user to know how much more they have to spend to get a free consultation
-    else:
-        print ("No free PT session for you!")
+    for item_id, quantity in bag.items():
+        plan = get_object_or_404(Plan, pk=item_id) #first get plan
+        total += quantity * plan.price #add qty*price to total
+        plan_count += quantity #increment plan count by quantity
+        bag_items.append({           #add dictionary to list of bag items containing id, quantity and plan object itself (gives us access to image etc)
+            'item_id': item_id,
+            'quantity': quantity,
+            'plan': plan,       #add plan object itself so we have access to all of the other fields when iterating through bag items
+        })
+
+
+    #if total < settings.FREE_PT_THRESHOLD:
+
+        #free_pt_delta = settings.FREE_PT_THRESHOLD - total #Allows user to know how much more they have to spend to get a free consultation
+    #else:
+        #print ("No free PT session for you!")
+
+    grand_total = total #need to declare grand_total variable, updates bag total as seen in base.html code
 
     context = {
         'bag_items':bag_items,
         'total': total,
         'plan_count': plan_count,
-        'free_pt_delta': free_pt_delta,
+        #ß'free_pt_delta': free_pt_delta,
         'free_pt_threshold': settings.FREE_PT_THRESHOLD,
         'grand_total': grand_total,
-        
     } #function returns dictionary called 'context'...add items to context so available in templates across the site
 
     return context #this is a context processor. It's purpose is to make this dictionary available to all templates across
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,20 @@ def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id): #item_id is id of plan
+    """ Add a number of sepcific plan to the shopping bag (note no plan is the same-tweaked and tailored) """
+
+    quantity = int(request.POST.get('quantity')) #get quantity from form and convert to integer-comes from template as string
+    redirect_url = request.POST.get('redirect_url') #get redirect so we know where to redirect at end of process
+    bag = request.session.get('bag', {}) #between view and form...session used to allow information to be stored until client and server finished communicating
+    #store shopping items in session, persists until user closes browser so they can continuously add things to bag w/o losing contents
+    #check if bag variable is there is it exists in session, if not initialise to empty dictionary
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity #if item is already in bag, increment quantity
+    else:
+        bag[item_id] = quantity
+    #product can be put in dictionary with quantity
+    request.session['bag'] = bag #overwrite variable in session with updated version
+    return redirect(redirect_url) 
