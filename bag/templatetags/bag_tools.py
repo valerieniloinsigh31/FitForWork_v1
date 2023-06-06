fe4731ff138bcef4e8971c7832c26f9a6d73bc57
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-06-06 09:18:30 +0000

    Update javascript link code in bag.html

--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -4,5 +4,5 @@ from django import template
 register = template.Library() #variable called register, instance of template.Library()...creating custom template tags-django docs
 
 @register.filter(name='calc_subtotal') #decorator
-def calc_subtotal(price, quantity): #function thate takes in price and quantity as parameters and returns their product
+def calc_subtotal(price, quantity): #function that takes in price and quantity as parameters and returns their product
     return price * quantity
