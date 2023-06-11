8143ed68418396cfbc625386c255a17fd9349cb1
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-06-11 18:58:16 +0000

    Add deployment configs

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fitforwork.wsgi:application
--- fitforwork/settings.py
+++ fitforwork/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-dzj(wxnaal4=qocut&1sp-sx-ua@c6y57wqcr^#a*%vjbeyr0*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-valerienilo-fitforworkv-nsseehzfyrz.ws-eu99.gitpod.io']
+ALLOWED_HOSTS = ['https://fitforwork.herokuapp.com/', 'localhost']
 
 
 # Application definition
@@ -117,12 +118,18 @@ WSGI_APPLICATION = 'fitforwork.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
