647fbfa411332517f3c9d223c6f8a0ca742ec52f
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-06-11 20:09:01 +0000

    Remove SECRET_KEY and set debug

--- fitforwork/settings.py
+++ fitforwork/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dzj(wxnaal4=qocut&1sp-sx-ua@c6y57wqcr^#a*%vjbeyr0*'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['fitforwork.herokuapp.com/', 'localhost']
 
